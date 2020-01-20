64176c876c92062f9ad71bab4aa67d60cc9ee2de
lkfo415579 <gm0648@hotmail.com>
2020-01-20 10:30:51 +0800

    up

--- download_illusts.py
+++ download_illusts.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import datetime
 if sys.version_info >= (3, 0):
     import imp
     imp.reload(sys)
@@ -21,23 +22,39 @@ _REQUESTS_KWARGS = {
 }
 
 def main():
-    aapi = AppPixivAPI(**_REQUESTS_KWARGS)
-    json_result = aapi.illust_ranking('day', date='2016-08-01')
+    # aapi = AppPixivAPI(**_REQUESTS_KWARGS)
+    aapi = AppPixivAPI()
+    aapi.login("lkfo415579", "wwq9032sa0")
+    # aapi.login("gm0648", "qq321520")
 
-    directory = "dl"
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    from_date = datetime.datetime.strptime(sys.argv[1], "%Y%m%d").date()
+    TOP = 30
+    r = 0
+    while(1):
+        c_date = from_date.strftime("%Y-%m-%d")
+        print ("=" * 100)
+        print ("Date : %s" % c_date)
+        print ("=" * 100)
+        json_result = aapi.illust_ranking('day', date=c_date)
 
-    # download top3 day rankings to 'dl' dir
-    for illust in json_result.illusts[:3]:
-        image_url = illust.meta_single_page.get('original_image_url', illust.image_urls.large)
-        print("%s: %s" % (illust.title, image_url))
-        # aapi.download(image_url)
+        directory = "dl"
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
-        url_basename = os.path.basename(image_url)
-        extension = os.path.splitext(url_basename)[1]
-        name = "illust_id_%d_%s%s" % (illust.id, illust.title, extension)
-        aapi.download(image_url, path=directory, name=name)
+        # download top3 day rankings to 'dl' dir
+        for i, illust in enumerate(json_result.illusts[:TOP]):
+            image_url = illust.meta_single_page.get('original_image_url', illust.image_urls.large)
+            print("%d:%s" % (i, illust.title))
+            # aapi.download(image_url)
+
+            url_basename = os.path.basename(image_url)
+            extension = os.path.splitext(url_basename)[1]
+            name = "%d_%s%s" % (illust.id, illust.title.replace("/", ""), extension)
+            aapi.download(image_url, path=directory, name=name)
+        from_date = from_date + datetime.timedelta(days=1)
+        r += 1
+        if r >= 356:
+            break
 
 if __name__ == '__main__':
     main()
